a90594d9aa1abaa5565c530e272b9ffc9ad37a4d
MUHİTTİN AKIN <muhittinbilmuh@gmail.com>
2022-02-28 14:48:28 +0300

    mukemelsayi

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MukemmelSayi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Kendisi hariç bütün pozitif çarpanları (tam bölenleri) toplamı, 
+             * yine kendisine eşit olan sayılara ”mükemmel sayı” denir. 
+             * Örneğin 28=1+2+4+7+14 ve 6=1+2+3   gibi. 
+             * Buna göre klavyeden girilen bir tam sayının “mükemmel sayı” olup olmadığını kontrol eden
+             * C# programını kodlarını yazınız */
+            int sayi, toplam = 0;
+
+         sayiGirisi:
+            Console.WriteLine("Lütfen Kontrol  Edilecek Sayıyı Giriniz:");
+            string okunan = Console.ReadLine();
+
+            // string sayi dönüşüm işlemleri için
+            if (!Int32.TryParse(okunan, out sayi) || sayi < 0)
+            {
+                Console.WriteLine("Girilen değer doğru formatta değil.Lütfen sayi giriniz...");
+                goto sayiGirisi;
+            }
+
+            for (int i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    toplam += i;
+                }
+            }
+
+            if (sayi == toplam)
+            {
+                Console.WriteLine("{0} Sayısı Mükemmel Sayıdır",sayi);
+                
+            }
+            else
+            {
+                Console.WriteLine("{0} Sayısı Mükemmel Sayı Değildir", sayi);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
